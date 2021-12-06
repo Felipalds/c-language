c1c7c6eb34ecbfd4c2d1263640c0e8bb96f1f38e
luizfelipefrosa <luizfelipefonsecarosa@gmail.com>
2021-12-06 10:06:37 -0300

    Triange

---
+++ trabalho-04/which_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a, b, c, isRetangle;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if((a + b) <= c || (a + c) <= b || (b + c) <= a)
+        printf("Invalido\n");
+    else{
+        printf("Valido-");
+        if(a == b && b == c)
+            printf("Equilatero\n");
+        else if(a != b && b != c)
+            printf("Escaleno\n");
+        else
+            printf("Isoceles\n");
+        printf("Retangulo:");
+        if(pow(a, 2) == pow(b, 2) + pow(c, 2) || pow(b, 2) == pow(a, 2) + pow(c, 2) || pow(c, 2) == pow(b, 2) + pow(a, 2))
+            printf("S\n");
+        else
+            printf("N\n");
+    }
+    return 0;
+}

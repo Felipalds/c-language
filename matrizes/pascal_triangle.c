f91e513b4b3403bfbb200454cb4b4c3018e36f76
luizfelipefrosa <luizfelipefonsecarosa@gmail.com>
2022-02-16 10:42:41 -0300

    Pascal Triangle

--- matrizes/pascal_triangle.c
+++ matrizes/pascal_triangle.c
@@ -7,25 +7,31 @@ int main(){
 
         x++;
         int matriz[x][x];
+        for(int linhas = 0; linhas < x; linhas++){
+            for(int colunas = 0; colunas < x; colunas++){
+                matriz[linhas][colunas] = 0;
+            }
+        }
 
-        
-
-        for(int c = 0; c < x; c++){
-            for(int g = 0; g < x; g++){
-                if(c == 0 && g == 0){
-                    matriz[0][0] = 1;
+        for(int linhas = 0; linhas < x; linhas++){
+            for(int colunas = 0; colunas < linhas; colunas++){
+                if(colunas == 0){
+                    matriz[linhas][colunas] = 1;
                 } else {
-                    matriz[c][g] = matriz[c - 1][g] + matriz[c - 1][g - 1];
+                    matriz[linhas][colunas] = matriz[linhas - 1][colunas] + matriz[linhas - 1][colunas - 1];
                 }
             }
         }
-
+        
+    
         for(int a = 0; a < x; a++){
-            for(int b = 0; b < x; b++){
-                printf("%d ", matriz[a][b]);
+            for(int b = 0; b < a; b++){
+                if(matriz[a][b] != 0)
+                    printf("%d ", matriz[a][b]);
             }
             printf("\n");
         }
+        
     }
 
     return 0;

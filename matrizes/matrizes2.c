e06ca487145edddc071a7e99ad1c9bd400cede19
Felipalds <luizfelipefonsecarosa@gmail.com>
2022-04-30 12:15:54 -0300

    matrix

--- matrizes/matrizes2.c
+++ matrizes/matrizes2.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
-int matriz[10][10]
+int M[2][2];
+
+int scanMat(int M[2][2], int lines, int kols)
+{
+    for(int l = 0; l < lines; l++)
+        for(int c = 0; c < kols; c++)
+            scanf("%d", &M[l][c]);
+}
+
+void printMat(int M[2][2], int lines, int kols)
+{
+    for(int l = 0; l < lines; l++)
+    {
+        printf("[");
+        for(int c = 0; c < kols; c++)
+        {
+            if(c != kols - 1)
+                printf("%d , ", M[l][c]);
+            else
+                printf("%d", M[l][c]);
+        }
+        printf("]\n");
+    }
+}
 
 
-
-int main(){
-
-    
+int main()
+{
+    int lines = 2, kols = 2;
+    scanMat(M, lines, kols);
+    printMat(M, lines, kols);
 
     return 0;
 }
